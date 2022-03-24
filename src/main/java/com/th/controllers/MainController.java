49ba455972d3b9decce89c80159d451be57f490c
khalil <khalillakhdharatc@gmail.com>
2022-03-24 11:39:58 +0100

    Initial commit

---
+++ src/main/java/com/th/controllers/MainController.java
@@ -0,0 +1,25 @@
+package com.th.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import com.th.classes.Person;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+	private static List<Person> persons = new ArrayList<Person>();
+	static { // static {} signifie l'auto initialisation
+	persons.add(new Person("Bill", "Gates")); // ajout dans la liste
+	persons.add(new Person("Steve", "Jobs"));
+	}
+	@GetMapping("")
+	public String getPersons()
+	{
+		return  "Persons";
+	}
+	
+	
+	
+}
